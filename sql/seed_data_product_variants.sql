387d5c3a156e4a74543b08acab9005ad4cc25347
Bhaskar Sarma <b.sarma1729@gmail.com>
2026-06-12 15:36:36 +0200

    insert seed data to product_variants

---
+++ sql/seed_data_product_variants.sql
@@ -0,0 +1,50 @@
+INSERT INTO product_variants
+(product_id, sku, variant_name, price, compare_at_price, cost_price)
+VALUES
+(1,  'APL-IP15-128-BLK', 'Black / 128GB',       899.99, 999.99, 700.00),
+(1,  'APL-IP15-256-BLK', 'Black / 256GB',       999.99, 1099.99, 780.00),
+
+(2,  'SMS-S24-128-GRY', 'Gray / 128GB',         849.99, 949.99, 670.00),
+(2,  'SMS-S24-256-GRY', 'Gray / 256GB',         949.99, 1049.99, 740.00),
+
+(3,  'LEN-LP5-16-SLV',  'Silver / 16GB RAM',  1199.99, 1349.99, 920.00),
+(3,  'LEN-LP5-32-SLV',  'Silver / 32GB RAM',  1399.99, 1549.99, 1080.00),
+
+(4,  'SON-WH1000XM5-BLK', 'Black',             349.99, 399.99, 240.00),
+(4,  'SON-WH1000XM5-SLV', 'Silver',            349.99, 399.99, 240.00),
+
+(5,  'NKE-AF1-42-WHT',  'EU 42 / White',       119.99, 139.99, 65.00),
+(5,  'NKE-AF1-43-WHT',  'EU 43 / White',       119.99, 139.99, 65.00),
+
+(6,  'ADS-ULTRABOOST-42', 'EU 42 / Black',     179.99, 199.99, 105.00),
+(6,  'ADS-ULTRABOOST-43', 'EU 43 / Black',     179.99, 199.99, 105.00),
+
+(7,  'LEVI-501-32-32',  '32W x 32L',            79.99,  99.99, 38.00),
+(7,  'LEVI-501-34-32',  '34W x 32L',            79.99,  99.99, 38.00),
+
+(8,  'UNI-TSHIRT-M-BLK', 'Medium / Black',      19.99,  24.99,  8.00),
+(8,  'UNI-TSHIRT-L-BLK', 'Large / Black',       19.99,  24.99,  8.00),
+
+(9,  'IKE-DESK-120-WHT', '120cm / White',      199.99, 249.99, 120.00),
+(9,  'IKE-DESK-160-WHT', '160cm / White',      249.99, 299.99, 155.00),
+
+(10, 'LOG-MXM3-GRY',     'Graphite',            99.99, 119.99, 58.00),
+(10, 'LOG-MXM3-WHT',     'White',               99.99, 119.99, 58.00),
+
+(11, 'NES-COFFEE-1KG',   '1kg Pack',            24.99, 29.99, 12.00),
+(12, 'LIP-GREEN-100',    '100 Bags',             9.99, 12.99,  4.00),
+
+(13, 'PS5-STD-EU',       'Standard Edition',   549.99, 599.99, 450.00),
+(14, 'XBX-X-EU',         '1TB Console',        499.99, 549.99, 410.00),
+
+(15, 'SAM-TV55-4K',      '55-inch',            699.99, 799.99, 540.00),
+(15, 'SAM-TV65-4K',      '65-inch',            999.99, 1149.99, 760.00),
+
+(16, 'CAN-EOSR10-BODY',  'Camera Body Only',   899.99, 999.99, 720.00),
+(16, 'CAN-EOSR10-KIT',   'With 18-45mm Lens', 1099.99, 1199.99, 860.00),
+
+(17, 'KINDLE-PW-16',     '16GB',               169.99, 189.99, 120.00),
+(18, 'FITBIT-C6-BLK',    'Black',              149.99, 179.99, 100.00),
+
+(19, 'TP-LINK-AX3000',   'AX3000 Router',       89.99, 109.99, 55.00),
+(20, 'SEAGATE-2TB-EXT',  '2TB External HDD',    79.99,  99.99, 52.00);
